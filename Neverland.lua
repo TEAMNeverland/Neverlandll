05746843b08e342bea4f9e0c213be3fe20ed59d8
TEAMNeverland <92953287+TEAMNeverland@users.noreply.github.com>
2021-10-25 22:51:11 +0300

    Update Neverland.lua

--- Neverland.lua
+++ Neverland.lua
@@ -11571,7 +11571,7 @@ keyboard.inline_keyboard = {
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(Texti).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 end
-if text == 'اعدادت البوت' then
+if text == 'اعدادات البوت' then
 local Texti = 'تستطيع تفعيل وتعطيل عبر الازرار'
 keyboard = {} 
 keyboard.inline_keyboard = {
